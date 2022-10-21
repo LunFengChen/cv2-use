ac1edd8a7468fccb31664dc94d91937cad14d416
XiaoFeng <2292990278@qq.com>
2022-10-21 17:25:24 +0800

    说明

--- codes/8_cv2算术操作.py
+++ codes/8_cv2算术操作.py
@@ -23,13 +23,12 @@ dst = cv2.addWeighted(img1, 1, img2, 1, 0)
 cv2.imshow('dst', dst), cv2.waitKey(0), cv2.destroyAllWindows()
 
 # 4. 位运算
-
-# I want to put logo on top-left corner, So I create a ROI
 rows, cols, channels = img2.shape
 roi = img1[0:rows, 0:cols]
 
-# Now create a mask of logo and create its inverse mask also
+
 img2gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+# 设定阈值
 ret, mask = cv2.threshold(img2gray, 175, 255, cv2.THRESH_BINARY)
 mask_inv = cv2.bitwise_not(mask)
 
--- readme.md
+++ readme.md
@@ -1,1 +1,5 @@
 本项目为opencv中文手册代码集合，若您感兴趣，请看codes下的代码
+
+# opencv教程参考
+- https://www.kancloud.cn/aollo/aolloopencv/262768
+- https://youcans.blog.csdn.net/?type=blog
